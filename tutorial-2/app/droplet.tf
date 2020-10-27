c51108db511b4fdc8c0eec7598d2458177f0fde4
Joe Perez <joseph.perez.la@gmail.com>
2020-10-26 21:24:41 -0700

    adding tutorial 2 starting point

---
+++ tutorial-2/app/droplet.tf
@@ -0,0 +1,16 @@
+data "digitalocean_ssh_key" "root" { 
+  name = "taccoform-tutorial"
+}
+
+resource "digitalocean_droplet" "web" {
+  image     = "ubuntu-20-04-x64"
+  name      = "web-burrito-prod"
+  region    = "sfo2"
+  size      = "s-1vcpu-1gb"
+  ssh_keys  = [data.digitalocean_ssh_key.root.id]
+  user_data = templatefile("templates/user_data_nginx.yaml", { hostname = "web-burrito-prod" })
+}
+
+output "droplet_public_ip" {
+  value = digitalocean_droplet.web.ipv4_address
+}
